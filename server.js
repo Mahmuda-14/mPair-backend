4c542c1cb90744ea5677b48b0e1c11c665f72768
Mahmuda-14 <mahmudaiiuc14@gmail.com>
2024-09-04 21:03:51 +0600

    added env

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ import express from "express";
 import mysql from "mysql";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+const PORT = process.env.PORT || 8081
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "signup",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME
 });
 
 
@@ -100,6 +100,6 @@ app.post('/login', (req, res) => {
 
 
 
-app.listen(8081, () => {
+app.listen(PORT, () => {
   console.log("Server is Running");
 });
